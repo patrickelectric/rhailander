3c5ff0542729656f152990ad1739631da08dade1
Patrick José Pereira <patrickelectric@gmail.com>
2021-03-28 12:02:05 -0300

    script: First version
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ src/script.rs
@@ -0,0 +1,24 @@
+use rhai::{plugin::*, Engine, EvalAltResult};
+
+use super::companion;
+use super::helper;
+use super::network;
+use super::system;
+
+pub fn run_script(path: &str) -> bool {
+    let mut engine = Engine::new();
+    engine.register_static_module("companion", exported_module!(companion::companion).into());
+    engine.register_static_module("helper", exported_module!(helper::helper).into());
+    engine.register_static_module("network", exported_module!(network::network).into());
+    engine.register_static_module("system", exported_module!(system::system).into());
+    engine.register_fn("run_rhai", run_script);
+
+    match engine.eval_file::<()>(std::path::PathBuf::from(path)) {
+        Ok(_) => true,
+        Err(error) => {
+            println!("Failed to run script: {}", path);
+            println!("Reason: {:?}", error);
+            return false;
+        }
+    }
+}
