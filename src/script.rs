f93ec4e23d6c0e05f10434dc96daf4984f83636d
Patrick José Pereira <patrickelectric@gmail.com>
2021-03-28 12:41:57 -0300

    script: Add env function
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/script.rs
+++ src/script.rs
@@ -5,12 +5,27 @@ use super::helper;
 use super::network;
 use super::system;
 
+pub fn env(name: &str) -> String {
+    match std::env::var(name) {
+        Ok(value) => value,
+        Err(error) => {
+            println!(
+                "Failed to fetch environment variable ({}), error: {:?}",
+                name, error
+            );
+            return String::new();
+        }
+    }
+}
+
 pub fn run_script(path: &str) -> bool {
     let mut engine = Engine::new();
     engine.register_static_module("companion", exported_module!(companion::companion).into());
     engine.register_static_module("helper", exported_module!(helper::helper).into());
     engine.register_static_module("network", exported_module!(network::network).into());
     engine.register_static_module("system", exported_module!(system::system).into());
+
+    engine.register_fn("env", env);
     engine.register_fn("run_rhai", run_script);
 
     match engine.eval_file::<()>(std::path::PathBuf::from(path)) {
