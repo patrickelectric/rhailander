ee35431cdb9490a66276d4a0f4c9942c8c421517
Patrick José Pereira <patrickelectric@gmail.com>
2021-03-30 17:10:31 -0300

    Add termimad with mprint function to print markdown
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/script.rs
+++ src/script.rs
@@ -1,4 +1,5 @@
 use rhai::{plugin::*, Engine, EvalAltResult};
+use termimad;
 
 use super::companion;
 use super::helper;
@@ -18,6 +19,10 @@ pub fn env(name: &str) -> String {
     }
 }
 
+pub fn markdown_print(string: &str) {
+    termimad::print_text(string);
+}
+
 pub fn run_script(path: &str) -> bool {
     let mut engine = Engine::new();
     engine.register_static_module("companion", exported_module!(companion::companion).into());
@@ -27,6 +32,7 @@ pub fn run_script(path: &str) -> bool {
 
     engine.register_fn("env", env);
     engine.register_fn("run_rhai", run_script);
+    engine.register_fn("mprint", markdown_print);
 
     match engine.eval_file::<()>(std::path::PathBuf::from(path)) {
         Ok(_) => true,
